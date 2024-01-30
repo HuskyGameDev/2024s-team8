b3bcd1b6d26441106088f06c85e924ea0086bad5
AnakynSaylor <ajsaylor@mtu.edu>
2024-01-29 20:45:29 -0500

    Made play button work
    Connected play button to change to the Practice scene

--- main.gd
+++ main.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Practice.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
