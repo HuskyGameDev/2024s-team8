210235a56f4a3bd2e527e94af9db2c169158ff20
AnakynSaylor <ajsaylor@mtu.edu>
2024-02-15 17:21:05 -0500

    Added the assets and changed sprite of the player character
    Did not add animation to code

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Practice.tscn")
+	get_tree().change_scene_to_file("res://Practicing.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
