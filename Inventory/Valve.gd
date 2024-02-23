f6ce5af04f17119d5eb62e9e25376288bc01a897
Tyler Gingras <tsgingra@mtu.edu>
2024-02-23 00:04:07 -0500

    Preparing to add valve minigame interface and gameplay
    Preparing assets, fixing interactionmanager bugs, and more, in order to prepare for the minigame interaface and valve minigame

---
+++ Inventory/Valve.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	StageManager.changeCamera(1000)
+	interaction_area.interact = Callable(self, "_on_interact")
+	pass # Replace with function body.
+
+
+func _on_interact():
+	queue_free()
+	pass
--- Scenes/research1.gd
+++ Scenes/research1.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@onready var player = get_tree().get_first_node_in_group("Player")
 var HasLeft = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
