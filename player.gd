a8a1fb8fbf01a6d9569d81b9085f2a5b437db274
Luke L <lmleinon@mtu.edu>
2024-02-25 00:51:52 -0500

    Modified Animation System
    -Added 2D Sprite
    -Added AnimationPlayer
    -Added AnimationTree
    -Added char-Walk-placeholder: To combine all walk animations into a single sheet.
    
    Animation is now decided based on the velocity vector for simplicity.
    Animation now pauses when the pause menu is open.

--- player.gd
+++ player.gd
@@ -2,12 +2,16 @@ extends CharacterBody2D
 
 @export var playerSpeed = 350
 @onready var animation = get_node("AnimatedSprite2D")
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 @export var pauseMenu : PackedScene
 
 var screen_size
 var hasAttention = true
 var pause
 var ValveMinigame = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +21,8 @@ func _ready():
 	get_node("Control").get_node("Door_is_locked").hide()
 
 func _swap_attention():
-	#print("Before: "+str(hasAttention))
 	hasAttention = !hasAttention
-	#print("Before: "+str(hasAttention))
+	animationTree.set("active", hasAttention)
 
 func _process(_delta):
 	if hasAttention:
@@ -49,7 +52,13 @@ func _physics_process(delta):
 		if Input.is_action_pressed("UP"):
 			velocity.y -= 1
 			animation.play("Walk_Forward")
-				
+			
+		if velocity != Vector2.ZERO:
+			animationTree.set("parameters/Idle/blend_position", velocity.normalized())
+			animationTree.set("parameters/Moving/blend_position", velocity.normalized())
+			animationState.travel("Moving")
+		else:
+			animationState.travel("Idle")
 			
 		if Input.is_action_just_released("DOWN") or Input.is_action_just_released("UP") or Input.is_action_just_released("LEFT") or Input.is_action_just_released("RIGHT"):
 			animation.stop()
