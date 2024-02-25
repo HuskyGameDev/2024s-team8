c96d13264e569cbd116d71bb7d7dbb58cd660a65
Tyler Gingras <tsgingra@mtu.edu>
2024-02-25 12:47:02 -0500

    Valve game has win condition
    Valve1 = 135 degrees clockwise (pressing "S")
    Valve2 = 90 degrees clockwise
    Valve3 = 45 degrees clockwise
    Each has + or - 15 degree room for error.
    Works in both directions.

--- Scenes/ValveGame.gd
+++ Scenes/ValveGame.gd
@@ -12,8 +12,30 @@ extends Control
 @onready var ArrowArray = [Arrow1, Arrow2, Arrow3]
 @onready var index = 0
 
+
+func ConvertDegrees(degrees):
+	var newDegrees = int(degrees) % 360
+	if newDegrees < 0:
+		newDegrees = 360 + newDegrees
+	return newDegrees
+
 func _process(_delta):
 	RotateValve()
+	var Valve1Rotation = ConvertDegrees(ValveArray[0].rotation_degrees)
+	var Valve2Rotation = ConvertDegrees(ValveArray[1].rotation_degrees)
+	var Valve3Rotation = ConvertDegrees(ValveArray[2].rotation_degrees)
+	
+	if Valve1Rotation > 120 && Valve1Rotation < 145:
+		if Valve2Rotation > 75 && Valve2Rotation < 100:
+			if Valve3Rotation > 30 && Valve3Rotation < 60:
+				player.ValveMinigame = false
+				await get_tree().create_timer(2).timeout
+				queue_free()
+				player._swap_attention()
+				pass
+			pass
+		pass
+		
 	if Input.is_action_just_pressed("MENU"):
 		queue_free()
 		player._swap_attention()
@@ -33,7 +55,7 @@ func RotateValve():
 				index = -1
 			index += 1
 			ArrowArray[index].visible = true
-			pass
+			
 		else:
 			if Input.is_action_just_pressed("LEFT"):
 				ArrowArray[index].visible = false
@@ -41,7 +63,7 @@ func RotateValve():
 					index = 3
 				index -= 1
 				ArrowArray[index].visible = true
-				pass
+				
 	pass
 
 
