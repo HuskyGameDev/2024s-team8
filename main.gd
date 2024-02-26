b8714fac9be518a347dd9411d4b98b75d022d83a
Luke L <lmleinon@mtu.edu>
2024-02-26 13:01:28 -0500

    Added Intro Animation
    Designed and implemented intro animation of "the pod" flying through space approaching the "research ship".
    
    The names are in "" because I do not have sprites for such so they are both represented by the default Godot Icon.

--- main.gd
+++ main.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 
 func _on_play_pressed():
-	const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
-	StageManager.changeScene(COMMAND_DECK, 144, 128)
-	StageManager.changeCamera(304)
+	get_tree().change_scene_to_file("res://intro.tscn")
+	#const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
+	#StageManager.changeScene(COMMAND_DECK, 144, 128)
+	#StageManager.changeCamera(304)
 
 func _on_quit_pressed():
 	get_tree().quit()
