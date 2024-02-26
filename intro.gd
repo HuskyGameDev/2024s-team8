90f1dd7552d28deb4e4b11ea0720d363d817f3f1
Luke L <lmleinon@mtu.edu>
2024-02-26 13:16:07 -0500

    Hotfix Player Scene
    GitHub left lines in the Player Scene that caused it problems I removed it, hopefully there aren't any more problems.

--- intro.gd
+++ intro.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-func _on_animation_player_animation_finished(anim_name):
+func _on_animation_player_animation_finished(_anim_name):
 	const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
 	StageManager.changeScene(COMMAND_DECK, 144, 128)
 	StageManager.changeCamera(304)
