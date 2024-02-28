91f5d39ba5fe59fa2f5aaa474e64de50b8f95811
AnakynSaylor <ajsaylor@mtu.edu>
2024-02-28 13:28:43 -0500

    Added a way to skip intro using esc

--- intro.gd
+++ intro.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+func _process(delta):
+	if Input.is_action_just_pressed("MENU"):
+		const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
+		StageManager.changeScene(COMMAND_DECK, 144, 128)
+		StageManager.changeCamera(304)
+	
 func _on_animation_player_animation_finished(_anim_name):
 	const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
 	StageManager.changeScene(COMMAND_DECK, 144, 128)
