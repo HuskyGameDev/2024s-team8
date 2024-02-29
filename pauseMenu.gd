76f7faf9490b87db13b267c54f6273e6c5ed8332
AnakynSaylor <ajsaylor@mtu.edu>
2024-02-29 00:28:51 -0500

    Small changes to mini map

--- pauseMenu.gd
+++ pauseMenu.gd
@@ -3,11 +3,11 @@ extends Control
 func _on_resume_pressed():
 	queue_free()
 	
-	
 func _on_quit_pressed():
 	get_tree().quit()
 
 func _process(_delta):
+	
 	if Input.is_action_just_pressed("MENU"):
 		_on_resume_pressed()
 
