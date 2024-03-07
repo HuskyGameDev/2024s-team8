95ceefeabada08ec1b73f3f19946249156fe73c4
haengel@mtu.edu <155788762+haileyen27@users.noreply.github.com>
2024-03-07 17:37:23 -0500

    Revert "Merge branch 'main' of https://github.com/HuskyGameDev/2024s-team8"
    This reverts commit 48b78f16c97faa3a611bfddbf021873cdd15556f, reversing
    changes made to 1ebec5b948940befce739c5c6ae0d25c74d0665e.

--- Scenes/Vent.gd
+++ Scenes/Vent.gd
@@ -9,12 +9,9 @@ func _ready():
 
 
 func _on_interact():
-	const POWER_ROOM = preload("res://Scenes/Second floor rooms/power_room.tscn")
 	if PositionManager.HasCrowbar:
 		PositionManager.HasOpenedVent = true
 		get_node("Sprite2D").hide()
-		StageManager.changeScene(POWER_ROOM, 184, 120)
-		StageManager.changeCamera(304)
-		StageManager.scene_change = true
+
 
 
