4e9692a8f8093e320ddabaa143f760b282a9c200
Tyler Gingras <tsgingra@mtu.edu>
2024-03-10 17:44:50 -0400

    Locker Hiding Mechanic (for player)
    Locker mechanic is interactable, press F to go in, and then after 1 second you have the option to press F again to leave. Still need hiding + monster interaction but thats when the monster ai exists.

---
+++ Scenes/Main floor rooms/Locker.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var speech_sound = preload("res://Assets/voice_sans.mp3")
+
+var InLocker = false
+
+const lines: Array[String] = [
+	"I'm going to hide in this locker!"
+]
+
+const lines2: Array[String] = [
+	"I'm going to leave this locker!"
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+	pass # Replace with function body.
+
+
+func _on_interact():
+	if !InLocker:
+		player._swap_attention()
+		DialogManager.start_dialog(global_position, lines, speech_sound, false)
+		await DialogManager.dialog_finished
+		player.hide()
+		InLocker = true
+	
+		await get_tree().create_timer(1.0).timeout
+	
+	if InLocker:
+		if Input.is_action_just_pressed("INTERACT"):
+			DialogManager.start_dialog(global_position, lines2, speech_sound, false)
+			await DialogManager.dialog_finished
+			player.show()
+			player._swap_attention()
+			InLocker = false
+		
+	
+
+
