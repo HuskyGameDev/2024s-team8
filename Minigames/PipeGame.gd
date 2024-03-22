551d0066ca2f3f8cbcfff9b4a4bca7c6949471ed
Tyler Gingras <tsgingra@mtu.edu>
2024-03-22 18:15:23 -0400

    PipeGame Complete
    Pipe game implementation and win condition are complete.
    Still need act 1 -> act 2 animation and sequence and information stored in PositionManager.

--- Minigames/PipeGame.gd
+++ Minigames/PipeGame.gd
@@ -7,6 +7,7 @@ extends Control
 @onready var Row3Node = get_tree().get_first_node_in_group("Row3")
 @onready var Row4Node = get_tree().get_first_node_in_group("Row4")
 @onready var Row5Node = get_tree().get_first_node_in_group("Row5")
+@onready var RowNodes = [Row1Node, Row2Node, Row3Node, Row4Node, Row5Node]
 
 @onready var Row1 = [Row1Node.get_child(0), Row1Node.get_child(1), Row1Node.get_child(2), Row1Node.get_child(3), Row1Node.get_child(4)]
 @onready var Row2 = [Row2Node.get_child(0), Row2Node.get_child(1), Row2Node.get_child(2), Row2Node.get_child(3), Row2Node.get_child(4)]
@@ -17,24 +18,74 @@ extends Control
 
 var RowIndex = 0
 var ColIndex = 0
+var solved = false
 
 func _process(_delta):
-	if Input.is_action_just_pressed("INTERACT"):
-		Rows[RowIndex][ColIndex].rotation_degrees += 90
-	if Input.is_action_just_pressed("LEFT"):
-		ColIndex = ColIndex - 1 if ColIndex > 0 else 4
-	if Input.is_action_just_pressed("RIGHT"):
-		ColIndex = ColIndex + 1 if ColIndex < 4 else 0
-	if Input.is_action_just_pressed("UP"):
-		RowIndex = RowIndex - 1 if RowIndex > 0 else 4
-	if Input.is_action_just_pressed("DOWN"):
-		RowIndex = RowIndex + 1 if RowIndex < 4 else 0
-	Cursor.position = Rows[RowIndex][ColIndex].position
+	if !solved:
+		if Input.is_action_just_pressed("INTERACT"):
+			Rows[RowIndex][ColIndex].rotation_degrees += 90
+			RowNodes[RowIndex].rotatePipe(ColIndex+1)
+			if !((RowNodes[0].Pipe1[0] != true) or (RowNodes[0].Pipe1[3] != true)):
+				if (RowNodes[4].Pipe5[2] == true && RowNodes[4].Pipe5[1]):
+					if checkWinRecurse(1,0,3):
+						solved = true
+						await get_tree().create_timer(2).timeout
+						queue_free()
+						player._swap_attention()
+						print("solved whole thing")
+		
+		if Input.is_action_just_pressed("LEFT"):
+			ColIndex = ColIndex - 1 if ColIndex > 0 else 4
+		if Input.is_action_just_pressed("RIGHT"):
+			ColIndex = ColIndex + 1 if ColIndex < 4 else 0
+		if Input.is_action_just_pressed("UP"):
+			RowIndex = RowIndex - 1 if RowIndex > 0 else 4
+		if Input.is_action_just_pressed("DOWN"):
+			RowIndex = RowIndex + 1 if RowIndex < 4 else 0
+		Cursor.position = Rows[RowIndex][ColIndex].position
 	
 	if Input.is_action_just_pressed("MENU"):
 		queue_free()
 		player._swap_attention()
-	
 
+
+#Order, Left = 0, Up = 1, Right = 2, Down = 3
+func checkWinRecurse(RowIndex, ColIndex, FromDirection):
+	if RowIndex == 4 && ColIndex == 4:
+		return true
+	
+	if !RowNodes[RowIndex].Pipes[ColIndex][alternateDirection(FromDirection)]:
+		return false
+	
+	var directions = RowNodes[RowIndex].Pipes[ColIndex]
+	for n in 4:
+		if directions[n] && n != alternateDirection(FromDirection):
+			if n == 0 && ColIndex > 0:
+				if checkWinRecurse(RowIndex, ColIndex-1, n):
+					return true
+			elif n == 1 && RowIndex > 0:
+				if checkWinRecurse(RowIndex-1, ColIndex, n):
+					return true
+			elif n == 2 && ColIndex < 4:
+				if checkWinRecurse(RowIndex, ColIndex+1, n):
+					return true
+			elif n == 3 && RowIndex < 4:
+				if checkWinRecurse(RowIndex+1, ColIndex, n):
+					return true
+	return false
+
+func alternateDirection(Direction):
+	if Direction == 0:
+		return 2
+	elif Direction == 2:
+		return 0
+	elif Direction == 1:
+		return 3
+	elif Direction == 3:
+		return 1
+	
+	
+	
+	
 
 
---
+++ Minigames/Row4.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+#Direction Order: Left, Up, Right, Down
+var Pipe1 = [true, false, false, true]
+var Pipe2 = [true, false, true, false]
+var Pipe3 = [true, false, false, true]
+var Pipe4 = [true, false, false, true]
+var Pipe5 = [true, false, false, true]
+var Pipes = [Pipe1, Pipe2, Pipe3, Pipe4, Pipe5]
+
+func rotatePipe(PipeNumber):
+	if PipeNumber == 1:
+		var temp = Pipe1[3]
+		Pipe1[3] = Pipe1[2]
+		Pipe1[2] = Pipe1[1]
+		Pipe1[1] = Pipe1[0]
+		Pipe1[0] = temp
+	elif PipeNumber == 2:
+		var temp = Pipe2[3]
+		Pipe2[3] = Pipe2[2]
+		Pipe2[2] = Pipe2[1]
+		Pipe2[1] = Pipe2[0]
+		Pipe2[0] = temp
+	elif PipeNumber == 3:
+		var temp = Pipe3[3]
+		Pipe3[3] = Pipe3[2]
+		Pipe3[2] = Pipe3[1]
+		Pipe3[1] = Pipe3[0]
+		Pipe3[0] = temp
+	elif PipeNumber == 4:
+		var temp = Pipe4[3]
+		Pipe4[3] = Pipe4[2]
+		Pipe4[2] = Pipe4[1]
+		Pipe4[1] = Pipe4[0]
+		Pipe4[0] = temp
+	elif PipeNumber == 5:
+		var temp = Pipe5[3]
+		Pipe5[3] = Pipe5[2]
+		Pipe5[2] = Pipe5[1]
+		Pipe5[1] = Pipe5[0]
+		Pipe5[0] = temp
+
+
