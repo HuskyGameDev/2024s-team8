bfe8735c5a33a546562e25df4adaa6c32b260822
Tyler Gingras <tsgingra@mtu.edu>
2024-04-09 15:51:53 -0400

    Fix dialogue bug?

--- Dialog/text_box.gd
+++ Dialog/text_box.gd
@@ -29,7 +29,7 @@ func display_text(text_to_display: String, speech_sfx: AudioStream):
 	if size.x > MAX_WIDTH:
 		label.autowrap_mode = TextServer.AUTOWRAP_WORD
 		await resized
-		await resized
+		#await resized
 		custom_minimum_size.y = size.y
 	
 	label.text = ""
