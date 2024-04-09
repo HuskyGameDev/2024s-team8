5b206591feeb11d3ba8d3f09343b5b23efa5de48
haengel@mtu.edu <155788762+haileyen27@users.noreply.github.com>
2024-04-09 15:43:15 -0400

    Small fixes
    Fixed: some odd tiles in main hallway, disabled Heli in main hallway, fixed command deck door (changed the "if count>2" to "if count>1"), added a poster in security room (not implemented, needs dialog),

---
+++ Scenes/Second floor rooms/SecurityRoomPoster.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var speech_sound = preload("res://Assets/Dialogue blip5.mp3")
+
+
+const lines: Array[String] = [
+	"'In case of emergency, press the button to unlock stairwell doors.'"
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+	pass # Replace with function body.
+
+
+func _on_interact():
+	player._swap_attention()
+	DialogManager.start_dialog(global_position, lines, speech_sound, false)
+	await DialogManager.dialog_finished
+	player._swap_attention()
+	pass
+	
+	
--- Scenes/hallway_main.gd
+++ Scenes/hallway_main.gd
@@ -123,8 +123,9 @@ func _on_stairs_body_exited(_body):
 
 func _on_bridge_body_entered(body):
 	const COMMAND_DECK = preload("res://Scenes/Main floor rooms/command_deck.tscn")
+	HasLeft = true
 	count += 1
-	if body.name == "Player" && count > 2:
+	if body.name == "Player" && count > 1:
 		if PositionManager.Act != 1:
 			$Player.hasAttention = false
 			$Player/AnimationTree.set("active", false)
