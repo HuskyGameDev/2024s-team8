948d39d2611f3904145e2afa6cd4e5eac3d5436f
AnakynSaylor <ajsaylor@mtu.edu>
2024-11-04 19:54:34 -0500

    Greenhouse aniimation
    -player runs to marker 1
    -player faces toward the monster
    - player sprints out of the room

--- Scenes/Second floor rooms/Greenhouse/greenhouse.gd
+++ Scenes/Second floor rooms/Greenhouse/greenhouse.gd
@@ -3,11 +3,13 @@ extends Node2D
 
 @export var usingMarker2 = false
 @export var leavingRoom = false
+var vector = Vector2.ZERO
 
 @onready var marker = $Marker2D
 @onready var marker2 = $Marker2D2
 @onready var player = get_node("Player")
-
+var x
+var y
 
 func _ready() -> void:
 	player.backingUp = false
@@ -15,41 +17,51 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#print(player.animMove)
 	if PositionManager.hasActivatedHeli && !PositionManager.hasEscapedGreenhouse:
 		if Input.is_action_just_pressed("MENU"):
 			leave_room()
+	if !usingMarker2:
+		x = marker.position.x - player.position.x
+		y = marker.position.y - player.position.y
+		vector = Vector2(x,y)
+		
+	player.animVec = vector
 	if player.animMove && !usingMarker2:
-		var vector = Vector2.ZERO
-		var x = marker.position.x - player.position.x
-		var y = marker.position.y - player.position.y
-	
-		if absf(x) > 0.4:
-			vector.x = x
-		if absf(y) > 0.4:
-			vector.y = y
+		
+		if absf(vector.x) <= 1 && absf(vector.y) <= 1:
+			player.animationTree.set("parameters/Idle/blend_position", Vector2(0,1))
+			player.animVec = Vector2.ZERO
 			
-		player.animVec = vector
+		else:
+			x = marker.position.x - player.position.x
+			y = marker.position.y - player.position.y
+			vector = Vector2(x,y)
+		
+		#player.animVec = vector
+		#player.animVec = Vector2.ZERO
 	elif player.animMove && usingMarker2:
 		player.playerSpeed = 125
-		var vector = Vector2.ZERO
-		var x = marker2.position.x - player.position.x
-		var y = marker2.position.y - player.position.y
+		
+		if absf(vector.x) <= 1 && absf(vector.y) <= 1:
+			player.animVec = Vector2.ZERO
+			vector = Vector2.ZERO
+		else:
+			x = marker2.position.x - player.position.x
+			y = marker2.position.y - player.position.y
+			vector = Vector2(x,y)
+		#player.animVec = vector
+		#player.animVec = Vector2.ZERO
 	
-		if absf(x) > 0.5:
-			vector.x = x
-		if absf(y) > 0.5:
-			vector.y = y
-			
-		player.animVec = vector
 	
-	if leavingRoom:
-		leave_room()
 
 func _swap_marker():
+	var x = marker2.position.x - player.position.x
+	var y = marker2.position.y - player.position.y
+	vector = Vector2(x,y)
 	usingMarker2 = true
 
-func toggle_animMove():
-	player.animMove = !player.animMove
+
 
 func _on_to_hallway_body_entered(body: Node2D) -> void:
 	if (Input.is_action_pressed("LEFT") or Input.is_action_pressed("UP")) && body.name == "Player":
